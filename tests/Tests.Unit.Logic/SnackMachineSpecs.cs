3ab50b22182d465baaed6aaff3edf60963d64eaa
Robin Bette <robinbette@hotmail.com>
2022-11-18 13:34:47 +0100

    * use more static fields to simplify tests

--- tests/Tests.Unit.Logic/SnackMachineSpecs.cs
+++ tests/Tests.Unit.Logic/SnackMachineSpecs.cs
@@ -8,7 +8,7 @@ public class SnackMachineSpecs
     public void Return_money_empties_money_in_transaction()
     {
         var snackMachine = new SnackMachine();
-        snackMachine.InsertMoney(new Money(0, 0, 0, 1, 0, 0));
+        snackMachine.InsertMoney(Money.Dollar);
 
         snackMachine.ReturnMoney();
 
