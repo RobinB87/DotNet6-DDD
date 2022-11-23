162ba07fa8fc384eb0558dfc1c17c495ca4a93a9
Robin Bette <robinbette@hotmail.com>
2022-11-23 13:01:54 +0100

    Save after buying snacks. Code is temporarily in VM

--- src/UI/SnackMachineViewModel.cs
+++ src/UI/SnackMachineViewModel.cs
@@ -1,68 +1,78 @@
 ﻿using DDD.In.Practice.UI.Common;
 using Logic;
+using NHibernate;
 
 namespace UI;
 public class SnackMachineViewModel : ViewModel
 {
-	private readonly SnackMachine _snackMachine;
+    private readonly SnackMachine _snackMachine;
     public override string Caption => "Snack Machine";
 
-	private string _message = string.Empty;
-	public string Message
-	{
-		get { return _message; }
-		private set 
-		{ 
-			_message = value;
-			Notify();
-		}
-	}
+    private string _message = string.Empty;
+    public string Message
+    {
+        get { return _message; }
+        private set
+        {
+            _message = value;
+            Notify();
+        }
+    }
 
-	public string MoneyInTransaction => _snackMachine.MoneyInTransaction.ToString();
-	public Money MoneyInside => _snackMachine.MoneyInside + _snackMachine.MoneyInTransaction;
+    public string MoneyInTransaction => _snackMachine.MoneyInTransaction.ToString();
+    public Money MoneyInside => _snackMachine.MoneyInside + _snackMachine.MoneyInTransaction;
 
-	public Command InsertCentCommand { get; private set; }
-	public Command InsertTenCentCommand { get; private set; }
-	public Command InsertQuarterCommand { get; private set; }
-	public Command InsertDollarCommand { get; private set; }
-	public Command InsertFiveDollarCommand { get; private set; }
-	public Command InsertTwentyDollarCommand { get; private set; }
-	public Command ReturnMoneyCommand { get; private set; }
-	public Command BuySnackCommand { get; private set; }
+    public Command InsertCentCommand { get; private set; }
+    public Command InsertTenCentCommand { get; private set; }
+    public Command InsertQuarterCommand { get; private set; }
+    public Command InsertDollarCommand { get; private set; }
+    public Command InsertFiveDollarCommand { get; private set; }
+    public Command InsertTwentyDollarCommand { get; private set; }
+    public Command ReturnMoneyCommand { get; private set; }
+    public Command BuySnackCommand { get; private set; }
 
     public SnackMachineViewModel(SnackMachine snackMachine)
-	{
-		_snackMachine = snackMachine;
+    {
+        _snackMachine = snackMachine;
         InsertCentCommand = new Command(() => InsertMoney(Money.Cent));
         InsertTenCentCommand = new Command(() => InsertMoney(Money.TenCent));
         InsertQuarterCommand = new Command(() => InsertMoney(Money.Quarter));
         InsertDollarCommand = new Command(() => InsertMoney(Money.Dollar));
         InsertFiveDollarCommand = new Command(() => InsertMoney(Money.FiveDollar));
         InsertTwentyDollarCommand = new Command(() => InsertMoney(Money.TwentyDollar));
-		ReturnMoneyCommand = new Command(() => ReturnMoney());
-		BuySnackCommand = new Command(() => BuySnack());
+        ReturnMoneyCommand = new Command(() => ReturnMoney());
+        BuySnackCommand = new Command(() => BuySnack());
     }
 
-	private void InsertMoney(Money coinOrNote)
-	{
-		_snackMachine.InsertMoney(coinOrNote);
+    private void InsertMoney(Money coinOrNote)
+    {
+        _snackMachine.InsertMoney(coinOrNote);
         NotifyClient($"You have inserted: {coinOrNote}");
-	}
+    }
 
-	private void ReturnMoney()
-	{
-		_snackMachine.ReturnMoney();
+    private void ReturnMoney()
+    {
+        _snackMachine.ReturnMoney();
         NotifyClient("Money was returned");
     }
 
     private void BuySnack()
-	{
-		_snackMachine.BuySnack();
-		NotifyClient("You have bought a snack");
+    {
+        _snackMachine.BuySnack();
+        NotifyClient("You have bought a snack");
+
+        // Transaction is not required as there is a single operation anyway,
+        // but it is a good habit when adding to db
+        using (ISession session = SessionFactory.OpenSession())
+        using (ITransaction transaction = session.BeginTransaction())
+        {
+            session.SaveOrUpdate(_snackMachine);
+            transaction.Commit();
+        }
     }
 
-	private void NotifyClient(string message)
-	{
+    private void NotifyClient(string message)
+    {
         Message = message;
         Notify(nameof(MoneyInTransaction));
         Notify(nameof(MoneyInside));
