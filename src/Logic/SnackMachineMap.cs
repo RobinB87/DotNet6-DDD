8a2ea82ef98d8a8b2c62b4f60cd3b6cb7e7a7bb1
Robin Bette <robinbette@hotmail.com>
2022-11-23 08:44:15 +0100

    * add persistence logic

---
+++ src/Logic/SnackMachineMap.cs
@@ -0,0 +1,22 @@
+﻿using FluentNHibernate.Mapping;
+
+namespace Logic;
+public class SnackMachineMap : ClassMap<SnackMachine>
+{
+    public SnackMachineMap()
+    {
+        // Specify id map with Id method
+        Id(x => x.Id);
+
+        // Map only money inside property, not money in transaction
+        Component(x => x.MoneyInside, y =>
+        {
+            y.Map(x => x.OneCentCount);
+            y.Map(x => x.TenCentCount);
+            y.Map(x => x.QuarterCount);
+            y.Map(x => x.OneCentCount);
+            y.Map(x => x.FiveDollarCount);
+            y.Map(x => x.TwentyDollarCount);
+        });
+    }
+}
