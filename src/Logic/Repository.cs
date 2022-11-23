8a3f63778d2aba4cde6ebbe658ce7e1bc718501e
Robin Bette <robinbette@hotmail.com>
2022-11-23 16:51:47 +0100

    add comment

--- src/Logic/Repository.cs
+++ src/Logic/Repository.cs
@@ -6,6 +6,11 @@ public abstract class Repository<T>
 {
     public T GetById(long id)
     {
+        // Normally you would keep this alive, and inject in constructor
+        // but in this WPF application which works in a detached mode
+        // our VM's keep references to the domain entities
+        // therefore we have to detach them from the sessions in which
+        // they were created. Therefore, the session does not need to be kept alive
         using (ISession session = SessionFactory.OpenSession())
         {
             return session.Get<T>(id);
