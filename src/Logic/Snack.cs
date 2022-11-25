bd6e420cfc468cc26b0cc8c0e6239a3783bda684
Robin Bette <robinbette@hotmail.com>
2022-11-25 10:03:48 +0100

    use nameof

--- src/Logic/Snack.cs
+++ src/Logic/Snack.cs
@@ -5,10 +5,10 @@ public class Snack : AggregateRoot
 	// Predefined data, change is relatively rare
 	// The constructor is then made private
 	// Create a 'None' Snack according to the null design pattern
-	public static readonly Snack None = new Snack(0, "None");
-	public static readonly Snack Chocolate = new Snack(1, "Chocolate");
-	public static readonly Snack Soda = new Snack(2, "Soda");
-	public static readonly Snack Gum = new Snack(3, "Gum");
+	public static readonly Snack None = new Snack(0, nameof(None));
+	public static readonly Snack Chocolate = new Snack(1, nameof(Chocolate));
+	public static readonly Snack Soda = new Snack(2, nameof(Soda));
+	public static readonly Snack Gum = new Snack(3, nameof(Gum));
 
     public virtual string Name { get; } = string.Empty;
 
