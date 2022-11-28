9844a28f539aaed5904cdfda688be79643a5442f
Robin Bette <robinbette@hotmail.com>
2022-11-28 08:23:59 +0100

    * add first logic for atm * add unit test

---
+++ src/Logic/Atms/Atm.cs
@@ -0,0 +1,23 @@
+﻿using Logic.Common;
+using Logic.SharedKernel;
+
+namespace Logic.Atms;
+public class Atm : AggregateRoot
+{
+    public virtual Money MoneyInside { get; protected set; } = Money.None;
+    public virtual decimal MoneyCharged { get; protected set; }
+
+    public virtual void TakeMoney(decimal amount)
+    {
+        var output = MoneyInside.Allocate(amount);
+        MoneyInside -= output;
+
+        var amountWithCommission = amount + amount * 0.01m;
+        MoneyCharged += amountWithCommission;
+    }
+
+    public virtual void LoadMoney(Money money)
+    {
+        MoneyInside += money;
+    }
+}
---
+++ tests/Tests.Unit.Logic/Atms/AtmSpecs.cs
@@ -0,0 +1,18 @@
+﻿using Logic.Atms;
+using Logic.SharedKernel;
+
+namespace Tests.Unit.Logic.Atms;
+public class AtmSpecs
+{
+    [Fact]
+    public void Take_money_exchanges_money_with_commission()
+    {
+        var atm = new Atm();
+        atm.LoadMoney(Money.Dollar);
+
+        atm.TakeMoney(1m);
+
+        atm.MoneyInside.Amount.Should().Be(0m);
+        atm.MoneyCharged.Should().Be(1.01m);
+    }
+}
