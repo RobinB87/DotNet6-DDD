59ef97d3a316f32a5026ddc2ea0251dfb9ba6db6
Robin Bette <robinbette@hotmail.com>
2022-11-28 11:04:35 +0100

    define  new bounded context: management

---
+++ src/Logic/Management/HeadOffice.cs
@@ -0,0 +1,9 @@
+﻿using Logic.Common;
+using Logic.SharedKernel;
+
+namespace Logic.Management;
+public class HeadOffice : AggregateRoot
+{
+    public virtual decimal Balance { get; protected set; }
+    public virtual Money Cash { get; protected set; } = Money.None;
+}
